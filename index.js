a73c101f12caf55d2d8121873239e21a2f891183
fauzandotme <fauzan@mantolitoli.sch.id>
2017-04-12 22:48:04 +0700

    fix manual command line

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const ffmpeg = require('ffmpeg');
 const mediainfoParser = require("mediainfo-parser").parse;
 const exec = require('child_process').exec;
+const shellescape = require('shell-escape');
+const path = require('path');
 
-module.exports = {getSS, encode, info};
+module.exports = {getSS, encode, info, getSub};
 
 function info(videoPath) {
   return new Promise((resolve, reject) => {
-    exec(`mediainfo --Full --Output=XML "${videoPath}"`,{maxBuffer: 1024 * 5000}, (err, res) => {
+    let command = ['mediainfo', '--Full', '--Output=XML', videoPath];
+    command = shellescape(command);
+    exec(command,{maxBuffer: 1024 * 5000}, (err, res) => {
       if(err) reject(err);
       mediainfoParser(res, (err, obj) => {
         if(err) reject(err)
@@ -21,58 +24,60 @@ function encode(opt = {}) {
     let input = opt.input ? opt.input : reject({message: 'empty input file'});
     let output = opt.output ? opt.output : reject({message: 'empty output file'});
     let subtitle = opt.subtitle ? opt.subtitle : '';
-    let codec = opt.codec ? opt.codec : 'h264';
+    let codec = opt.codec ? opt.codec : 'libx264';
     let crf = opt.crf ? opt.crf : '30';
     let scale = opt.scale ? opt.scale : false;
     let logo = opt.logo ? opt.logo : false;
     let preset = opt.preset ? opt.preset : false;
     let overwrite = opt.overwrite ? opt.overwrite : false;
     let format = opt.format ? opt.format : 'mp4';
-    try {
-      var process = new ffmpeg(input);
-      process.then(function (video) {
-        if(codec) video.setVideoCodec(codec);
-        if(format) video.setVideoFormat(format) ;
+    let faststart = opt.faststart ? opt.faststart : false;
 
-        let vf = [];
-        if(subtitle) vf.push(`subtitles=${subtitle}`);
-        if(scale) vf.push(`scale=${scale}`);
-        if(logo) vf.push(`ass=${logo}`);
-        if(vf.length > 0) video.addCommand('-vf', `'${vf.join(', ')}'`);
-        if(preset) video.addCommand('-preset', preset);
-        if(crf) video.addCommand('-crf', crf);
-        if(overwrite) video.addCommand('-y', '')
 
-        video.save(output, function (error, file) {
-    			if(error) reject(parseError(error));
-    			resolve(file);
-    		});
-      }, function (err) {
-        reject(err);
-      });
-    } catch (e) {
-      reject(e);
-    }
+    let command = ['ffmpeg', '-i', input];
+    if(codec) Array.prototype.push.apply(command, ['-c:v', codec]);
+    if(format) Array.prototype.push.apply(command, ['-f', format]);
+    let vf = [];
+    if(subtitle) vf.push(`subtitles=${subtitle}`);
+    if(scale) vf.push(`scale=${scale}`);
+    if(logo) vf.push(`ass=${logo}`);
+    if(vf.length > 0) Array.prototype.push.apply(command, ['-vf', vf.join(', ')]);
+    if(preset) Array.prototype.push.apply(command, ['-preset', preset]);
+    if(crf) Array.prototype.push.apply(command, ['-crf', crf]);
+    if(overwrite) command.push('-y');
+    if(faststart) Array.prototype.push.apply(command, ['-movflags', 'faststart']);
+    command.push(output);
+    command = shellescape(command);
+    exec(command,{maxBuffer: 1024 * 5000}, (err, res) => {
+      if(err) reject(parseError(err));
+      resolve({fileName: path.basename(output), filePath: output});
+    })
   });
 }
 
 
 
-function getSS(input, output) {
+function getSS(input, output, time = false) {
+  if(!time) time = '00:01:00';
   return new Promise((resolve, reject) => {
-    try {
-      var process = new ffmpeg(input);
-    	process.then(function (video) {
-        video.fnExtractFrameToJPG(output, {number: 3, every_n_frames: '100'}, function(error, files) {
-          if(error) reject(error)
-          resolve(files);
-        } )
-    	}, function (err) {
-    		reject(err)
-    	});
-    } catch (e) {
-      reject(e);
-    }
+    let command = ['ffmpeg', '-ss',time,'-i',input,'-vframes','1','-vcodec','png','-an','-y', output];
+    command = shellescape(command);
+    exec(command,{maxBuffer: 1024 * 5000}, (err, res) => {
+      if(err) reject(parseError(err));
+      resolve({fileName: path.basename(output), filePath: output});
+    })
+  })
+}
+
+function getSub(input, output, track = false) {
+  return new Promise((resolve, reject) => {
+    if(!track) track = 0;
+    let command = ['ffmpeg', '-i', input, '-y', '-map',`0:s:${track}`, output];
+    command = shellescape(command);
+    exec(command,{maxBuffer: 1024 * 5000}, (err, res) => {
+      if(err) reject(parseError(err));
+      resolve({fileName: path.basename(output), filePath: output});
+    })
   })
 }
 
